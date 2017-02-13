3886e3437cdbe0a010e3ff372cbed29eae3fa449
David Baker <drbaker2@gmail.com>
2017-02-12 20:22:46 -0500

    back to search results

--- _src/cabinets_2/app/components/Listing/index.js
+++ _src/cabinets_2/app/components/Listing/index.js
@@ -13,6 +13,24 @@ import HeroSection from 'components/HeroSection';
 const Wrapper = styled.div`
 display:flex;
 flex-direction: column;
+
+.back-button {
+  margin-left: 10px;
+
+  .back-arrow {
+    margin 5px;
+    vertical-align: top;
+    line-height: 18px;
+  }
+
+  .back {
+    color: #767676;
+    font-size: 12px;
+
+    &:hover {
+      color: #c45500;
+    }
+}
 `;
 
 class Listing extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -23,6 +41,7 @@ class Listing extends React.Component { // eslint-disable-line react/prefer-stat
   render() {
     return (
       <Wrapper>
+        <p className="back-button"><span className="back-arrow">‹&nbsp;</span><a href="#" className="back">Back to search results for "cabinet"</a></p>
         <HeroSection {...this.props} />
         <ReviewList />
       </Wrapper>
